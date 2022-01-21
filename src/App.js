8fdaa093a8d7743e0059e0901909e7dedaecd57e
Rad13git <96777129+Rad13git@users.noreply.github.com>
2022-01-21 19:54:42 +0530

    Assignment 1 react
    21/1/2022

--- src/App.js
+++ src/App.js
@@ -1,41 +1,39 @@
-import React from 'react';
-import { ReactDOM } from 'react';
-import Expenses from './Components/Expenses';
 
+import Expenses from "./Components/Expenses";
 
-
-function App () {
-  const expenses = [
+function App(){
+  const expenses =[
     {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
+      id:'e1',
+      title:'Toilet Paper',
+      amount:'94.12',
+      date: new Date(2021,7,14),
     },
     {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
+      id:'e2',
+      title:'New Tv',
+      amount:'799.49',
+      date: new Date(2021,2,12),
     },
+    {
+      id:'e3',
+      title:'Car Insurance',
+      amount:'294.67',
+      date: new Date(2021,2,8),
+    },
+    {
+      id:'e4',
+      title:'New Desk(Wooden)',
+      amount:'450',
+      date: new Date(2021,5,120,)
+    }
   ];
 
-  
 
-  return (
+  return(
     <div>
-      <h2>Let's get started!</h2>
-      <Expenses item ={expenses} />
-      
+   <Expenses item={expenses}/>
     </div>
   );
-}
-
+} 
 export default App;
